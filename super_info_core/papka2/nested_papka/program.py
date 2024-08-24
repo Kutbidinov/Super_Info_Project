ca0959230132cb4a5c73f47811286a3ad785216f
madaz1movsa <166149668+Kutbidinov@users.noreply.github.com>
2024-08-24 16:45:06 +0600

    Changes1

---
+++ super_info_core/papka2/nested_papka/program.py
@@ -0,0 +1,20 @@
+print("Hello Danislan!")
+
+
+a = int(input())
+
+b = int(input())
+
+c = int(input())
+
+
+
+if a + b > c and a + c > b and c + b > a:
+
+	print("Your Triangle exists.")
+
+
+
+else:
+	print("Your triangle don't exists.")
+
